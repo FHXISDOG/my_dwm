2d1c532e6bf6bf518f06e0308726bcff2f001b33
finger <fhxisdog@163.com>
2020-08-24 12:48:22 +0800

    chagne i3lock config

--- config.def.h
+++ config.def.h
@@ -63,7 +63,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *rofi[] = {"rofi", "-show", "drun", "-modi", "run,drun", "-show-icons", "-theme", "fancy"};
 static const char *termcmd[]  = { "st", NULL };
-static const char *lock[] = {"i3lock", "-i", "/home/finger/Wallpaper/wallpaper.png"};
+static const char *lock[] = {"i3lock", "-i", "/home/finger/Wallpaper/wallpaper.png", "-t"};
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = rofi} },
